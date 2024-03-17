808d21371347f0395e92e63b0b82b5bd960c2b2d
axhvn <ayhanannaguliew999@gmail.com>
2024-03-17 16:47:06 +0500

    rahman

---
+++ ayhan/March 17.2024/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// let a = +prompt('a girzin', '');
+// let b = +prompt('b girzin', '');
+
+// for ()
+// console.log(sum)
+
+// --------------------------------------------------------------------
+
+let x = +prompt("san girzin", "");
+
+if (x < -2 || x > 2) {
+  console.log(2 * x);
+} else {
+  console.log(-3 * x);
+}
